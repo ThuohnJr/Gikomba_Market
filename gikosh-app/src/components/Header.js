d8e6bdf98513f3b91eb54f58494f934194784cc1
Thuohn Jr <thuohnjr@outlook.com>
2023-08-22 10:20:15 +0300

    adding more files to project

---
+++ gikosh-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import {Fragments} from 'react';
+
+function Header(){
+	return  (
+<>
+<header ="header">
+<img src="components/pics/bma3.png" alt="company logo" /> 
+<h1 className="main-title">Bungu's Mtumba Apparel</h1>
+<p className="mantra"> 'Your clothing choice selection'</p>
+</header>
+</>
+		)
+}
+export default Header;
